8841676bfbbc7c3b3c550371464b7a5b0b235f78
NebsNebbier <nebsnebbier@gmail.com>
2013-03-13 09:02:57 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nebbier::Application.config.secret_token = 'd39bbb8b02e9b4f56117807ea2147b4c2fa63c78a0bf80cf9a08db22f59444e1101a6666d10e6f454050b8a1da2eb86f9910a85c71c2c5b05dd73914f49337e7'
